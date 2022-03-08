77a0d3c180d72ba176a1ec6d13b80608662943c5
KGeri97 <kgeri999995@gmail.com>
2022-03-08 10:14:04 +0100

    Network
    Created a loopback client to easen the process of the people hosting the server, so they dont need to type in their own local ip.
    
    By pressing the appropriate button you are now loaded into the scene.

--- GameLab/Assets/Scripts/PreLoader.cs
+++ GameLab/Assets/Scripts/PreLoader.cs
@@ -1,33 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PreLoader : MonoBehaviour
 {
     public GameObject clientPrefab;
+    public GameObject loopBackclientPrefab;
     public GameObject serverPrefab;
     public string ipField;
 
     public void OnClickClient()
     {
         DontDestroyOnLoad(Instantiate(clientPrefab).gameObject);
-        //FindObjectOfType<BaseClient>().ipAddress = ipField;
-        //SceneManager.LoadScene("Lobby");
+        FindObjectOfType<BaseClient>().ipAddress = ipField;
+        SceneManager.LoadScene("CharacterTest");
     }
-    public void OnClickServer()
-    {
-        DontDestroyOnLoad(Instantiate(serverPrefab).gameObject);
-        //FindObjectOfType<BaseServer>().Init();
-        //SceneManager.LoadScene("Lobby");
-    }
+
+    //public void OnClickServer()
+    //{
+    //    DontDestroyOnLoad(Instantiate(serverPrefab).gameObject);
+    //    SceneManager.LoadScene("CharacterTest");
+    //}
+
     public void OnClickClientServer()
     {
         DontDestroyOnLoad(Instantiate(serverPrefab).gameObject);
-        //FindObjectOfType<BaseServer>().Init();
-        DontDestroyOnLoad(Instantiate(clientPrefab).gameObject);
+        DontDestroyOnLoad(Instantiate(loopBackclientPrefab).gameObject);
         //FindObjectOfType<BaseClient>().ipAddress = ipField;
-        //SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene("CharacterTest");
     }
 
     public void OnValueChangeAssignIP(string value)
