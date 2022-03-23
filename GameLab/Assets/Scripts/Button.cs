95dd232a518ce60907183b100cf845f78e22e57e
DHindriks <dl.hindriks@gmail.com>
2022-03-23 12:54:23 +0100

    Proto button behaviour

--- GameLab/Assets/Scripts/Button.cs
+++ GameLab/Assets/Scripts/Button.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,15 +7,36 @@ public class Button : MonoBehaviour
 
     public UnityEvent OnDeactivate;
 
-    // Start is called before the first frame update
-    void Start()
+    bool CheckInput;
+
+    bool Active;
+
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            CheckInput = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            CheckInput = false;
+        }
+    }
+
+    public void SetStatus(bool active)
+    {
+        Active = active;
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Interact"))
+        {
+
+        }
     }
 }
