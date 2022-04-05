dc1568b4cf215fb9d5804dc5e49bc1daf1e0754a
KGeri97 <kgeri999995@gmail.com>
2022-04-05 17:06:34 +0200

    Reverted the Input system and added the 2d player controller prefab

--- GameLab/Assets/Scripts/Player/NewPlayerMovement.cs
+++ GameLab/Assets/Scripts/Player/NewPlayerMovement.cs
@@ -1,60 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
-using UnityEngine.InputSystem;
+//using Mirror;
+//using UnityEngine.InputSystem;
 
-public class NewPlayerMovement : NetworkBehaviour
+public class NewPlayerMovement : MonoBehaviour
 {
 
-    Rigidbody2D rb;
-    [SerializeField] float speed = 75;
-    [SerializeField] float jumpForce = 5;
-    [SerializeField] int jumps = 1;
-    [SerializeField] int currentJumps;
+    //    Rigidbody2D rb;
+    //    [SerializeField] float speed = 75;
+    //    [SerializeField] float jumpForce = 5;
+    //    [SerializeField] int jumps = 1;
+    //    [SerializeField] int currentJumps;
 
-    bool grounded;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float checkRadius;
-    [SerializeField] LayerMask groundLayers;
+    //    bool grounded;
+    //    [SerializeField] Transform groundCheck;
+    //    [SerializeField] float checkRadius;
+    //    [SerializeField] LayerMask groundLayers;
 
-    void Start()
-    {    
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //    void Start()
+    //    {    
+    //        rb = GetComponent<Rigidbody2D>();
+    //    }
 
-    private void Update()
-    {
-        rb.velocity = new Vector2(rb.velocity.x / 4, rb.velocity.y);
-    }
+    //    private void Update()
+    //    {
+    //        rb.velocity = new Vector2(rb.velocity.x / 4, rb.velocity.y);
+    //    }
 
-    public void Movement(InputAction.CallbackContext context)
-    {
-        if (isLocalPlayer)
-        {
-            Vector2 inputVector = context.ReadValue<Vector2>();
-            rb.AddForce(new Vector2(inputVector.x, 0) * speed * Time.deltaTime, ForceMode2D.Force);
-        }
-    }
+    //    public void Movement(InputAction.CallbackContext context)
+    //    {
+    //        if (isLocalPlayer)
+    //        {
+    //            Vector2 inputVector = context.ReadValue<Vector2>();
+    //            rb.AddForce(new Vector2(inputVector.x, 0) * speed * Time.deltaTime, ForceMode2D.Force);
+    //        }
+    //    }
 
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (isLocalPlayer && context.started) 
-        {
-            resetJumps();
-            if (currentJumps > 0 )
-            {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                currentJumps--;
-            }
-        }
-    }
+    //    public void Jump(InputAction.CallbackContext context)
+    //    {
+    //        if (isLocalPlayer && context.started) 
+    //        {
+    //            resetJumps();
+    //            if (currentJumps > 0 )
+    //            {
+    //                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    //                currentJumps--;
+    //            }
+    //        }
+    //    }
 
-    private void resetJumps()
-    {
-        if (Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayers))
-        {
-            currentJumps = jumps;
-        }
-    }
+    //    private void resetJumps()
+    //    {
+    //        if (Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayers))
+    //        {
+    //            currentJumps = jumps;
+    //        }
+    //    }
 }
