20791c032718df79727b90d1fae712882d244b8b
KGeri97 <kgeri999995@gmail.com>
2022-05-25 08:32:18 +0200

    Desetroys itself after 20 seconds

--- GameLab/Assets/Scripts/Powerups/BehaviorRollingPin.cs
+++ GameLab/Assets/Scripts/Powerups/BehaviorRollingPin.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BehaviorRollingPin : MonoBehaviour
 {
+    private void Start()
+    {
+        Destroy(gameObject, 20);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "player")
