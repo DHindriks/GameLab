931c5f3f86587b79f979f9703e1c0ce82e17f275
KGeri97 <kgeri999995@gmail.com>
2022-05-29 22:24:10 +0200

    Teammates are now not colliding

--- GameLab/Assets/Scripts/LobbyTeamAssign.cs
+++ GameLab/Assets/Scripts/LobbyTeamAssign.cs
@@ -11,5 +11,24 @@ public class LobbyTeamAssign : MonoBehaviour
         ActorTeam actor = collision.gameObject.GetComponent<ActorTeam>();
         actor.Team = team;
         actor.AssignTeam(team);
+
+        switch (team)
+        {
+            case Teams.None:
+                collision.gameObject.layer = 20;
+                break;
+            case Teams.Red:
+                collision.gameObject.layer = 21;
+                break;
+            case Teams.Green:
+                collision.gameObject.layer = 22;
+                break;
+            case Teams.Blue:
+                collision.gameObject.layer = 23;
+                break;
+            case Teams.Yellow:
+                collision.gameObject.layer = 24;
+                break;
+        }
     }
 }
